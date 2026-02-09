af118a20e0ea9bc620d5c4ac4b743498bc587247
zed <asyed7372@gmail.com>
2026-02-10 00:30:29 +0530

    feat: added search functionality for hotels

---
+++ src/main/java/com/notzed/airbnbapp/controller/HotelBrowseController.java
@@ -0,0 +1,31 @@
+package com.notzed.airbnbapp.controller;
+
+import com.notzed.airbnbapp.dto.HotelDto;
+import com.notzed.airbnbapp.dto.HotelSearchRequest;
+import com.notzed.airbnbapp.entity.Inventory;
+import com.notzed.airbnbapp.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/hotels")
+@RequiredArgsConstructor
+public class HotelBrowseController {
+
+    private final InventoryService inventoryService;
+
+    @GetMapping("/search")
+    public ResponseEntity<Page<HotelDto>> searchHotels(@RequestBody HotelSearchRequest hotelSearchRequest){
+
+        Page<HotelDto> page = inventoryService.searchHotels(hotelSearchRequest);
+        return ResponseEntity.ok(page);
+    }
+
+}
